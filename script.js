9637021588b661f7a7b7a7d78e528aa7dc53a074
Hritvik mohan <126161202+hritvik715@users.noreply.github.com>
2025-05-23 20:05:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyBnR9-VCe7uNMz-hTsvAugx_JfpBayQ3Bk";
+const GOOGLE_API_KEY = "AIzaSyBXfZCg_Z-MIgaItDI-YpJfpNMqIvqLyGI";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
